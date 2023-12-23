8ca00ee617fc81b71efe1bb048ee67114c9ad5d6
RicardoEgas <rj_egas@hotmail.com>
2023-12-23 02:33:36 +0100

    Add react and redux files

--- app/controllers/greetings_controller.rb
+++
@@ -1,8 +0,0 @@
-# app/controllers/greetings_controller.rb
-class GreetingsController < ApplicationController
-    def random_greeting
-      greetings = ['Hello!', 'Hi!', 'Greetings!', 'Hola!', 'Bonjour!']
-      render json: { message: greetings.sample }
-    end
-  end
-  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-# config/routes.rb
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  namespace :api do
+    get 'greetings/random', to: 'greetings#random_greeting'
+  end
   root 'static_pages#home'
-  get '/api/random_greeting', to: 'greetings#random_greeting'
 end
